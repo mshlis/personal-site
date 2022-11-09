458784c2a2c04c7a91680922a596407886478a73
Michael Shliselberg <mshlis@MacBook-Pro-2.local>
2022-11-09 14:42:07 -0500

    fixing links on main page

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 
 import Main from '../layouts/Main';
 
+const resume = `${process.env.PUBLIC_URL}/Resume_Michael_Shliselberg_11_8_22.pdf`;
+
 const Index = () => (
   <Main
     description={'Michael Shliselberg\'s personal website. PhD student researching ways to use AI to detect and combat Mis/Dis-information campaigns'}
@@ -17,8 +19,9 @@ const Index = () => (
         </div>
       </header>
       <p> I plan to use this site as a digital resume and outlet of sorts. Please feel free to read
-        <Link to="/about"> about me</Link>, about <Link to="/about">my experience</Link>, or check out my
-        <Link to="/about"> blog posts</Link>. You can also download my <Link to="/about">Resume</Link> or
+        <Link to="/about"> about me</Link>, about <Link to="/resume">my experience</Link>, or check out my
+        <Link to="/projects"> blog posts</Link>. You can also download my
+        <a href={resume} download={resume}> Resume</a> or
         {' '}look at my <Link to="/contact">Contact Information</Link> in case you want to reach me.
       </p>
     </article>
