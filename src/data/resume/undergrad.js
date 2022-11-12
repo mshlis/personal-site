88afdf2e8f42c956bdf5b497c799a2f2ab792cf7
Michael Shliselberg <mshlis@MacBook-Pro-2.local>
2022-11-11 20:04:17 -0500

    added more undergrad projects

--- src/data/resume/undergrad.js
+++ src/data/resume/undergrad.js
@@ -41,5 +41,35 @@ const undergrad = [
     ],
     year: 'Summer 2015',
   },
+  {
+    type: 'Project',
+    title: 'Text Detoxifier',
+    points: [
+      'Finds all versions of “f***” and replaces it with a word that is less explicit but retains sentences original meaning',
+      'Programmed different models such as a Bag of Words approach, Multiclass Logistic Regression, clustering based off feature vectors, and a skip-gram with different length spans',
+      'Annotated scraped data, and used simple active learning approach to maximize annotations impacts',
+    ],
+    year: 'Fall 2017',
+  },
+  {
+    type: 'Project',
+    title: 'Carvana Car-Masking Competition',
+    points: [
+      'Participated in competition for creating a pixel mask determining where a car is in a picture',
+      'Used models that combined concepts from Inception-Resnet and U-nets',
+      'Pretrained on errors from previous model to transfer features on structured components not found in basic training',
+    ],
+    year: 'Summer 2017',
+  },
+  {
+    type: 'Project',
+    title: 'Base Run Application',
+    points: [
+      'Developed an application in which users locally play a capture the flag type game using their mobile geolocations.',
+      'Implemented the backend server side in C++ utilizing RPC’s to communicate with the java client',
+      'Described deadlines and requirements for team as Development lead',
+    ],
+    year: 'Fall 2016',
+  },
 ];
 export default undergrad;
